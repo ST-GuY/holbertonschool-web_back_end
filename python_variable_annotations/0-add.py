ef92a6367b1045e00f95284334b5828ca850441b
ST-GuY <estebanlvy23@gmail.com>
2025-11-24 09:39:20 +0100

    add doc ...

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-
 """Module that defines the add function with type annotations.
 """
 
 
 def add(a: float, b: float) -> float:
+    """Return the sum of two floats.
+
+    Args:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        float: the sum of a and b
+    """
     return a + b
