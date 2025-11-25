c04bc572accdb56c18cc7beb0960bec1b8db07a1
ST-GuY <estebanlvy23@gmail.com>
2025-11-25 14:41:38 +0100

    doc

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,14 +2,28 @@
 import time
 import asyncio
 
-
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n, max_delay):
+    """
+    Mesure le temps moyen d'exécution de la coroutine wait_n.
+
+    Cette fonction :
+    - démarre un chronomètre,
+    - exécute wait_n(n, max_delay) de manière synchrone grâce à asyncio.run(),
+    - calcule le temps total écoulé,
+    - renvoie le temps moyen par coroutine (temps_total / n).
+
+    Args:
+        n (int): Nombre de tâches asynchrones à exécuter.
+        max_delay (int): Temps maximal (en secondes) pour chaque délai généré.
+
+    Returns:
+        float: Temps moyen d'exécution par coroutine.
+    """
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
     temps_total = end - start
     return temps_total / n
-
