3ee3fb68cd38862924632c21cbc9f895f7417a1f
ST-GuY <estebanlvy23@gmail.com>
2025-11-26 11:25:47 +0100

    doc

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,11 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+"""Génère 10 valeurs aléatoires (float entre 0 et 10)."""
+
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Génère 10 valeurs aléatoires (float entre 0 et 10).
     À chaque itération, attend 1 seconde de façon asynchrone puis yield la valeur.
     """
     for _ in range(10):
