f2b5648f407fee2778b0ee7b0feb3fe553deb8f8
ST-GuY <estebanlvy23@gmail.com>
2025-11-27 15:49:12 +0100

    f

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_comprehension
 
 
 async def async_comprehension() -> list[float]:
