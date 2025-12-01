aa05797c54db5e47ea19eb8e93d0de6a9c6cad9b
ST-GuY <estebanlvy23@gmail.com>
2025-12-01 09:54:53 +0100

    simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def index_range(page, page_size) -> tuple:
+    """
+    Retourne un tuple (start, end) correspondant
+    à la plage d'index pour la pagination.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
