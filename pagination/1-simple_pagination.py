d82799fd1f23d8302acb112bbcbb3d6d51a1794e
ST-GuY <estebanlvy23@gmail.com>
2025-12-01 11:10:35 +0100

    assert

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -30,6 +30,5 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        start = (page - 1) * page_size
-        end = page + page_size
-        return (start, end)
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
