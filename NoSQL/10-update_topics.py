c3d4c133f81a34c332cd90a3c13a3c56edb68071
ST-GuY <estebanlvy23@gmail.com>
2025-12-03 11:12:46 +0100

    update

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,9 +6,17 @@ Function that changes all topics of a school document based on the name:
 
 def update_topics(mongo_collection, name, topics):
     """
-    Function that changes all topics of a school document based on the name:
+    Modifie la liste des topics d'une école dans la collection.
+
+    Args:
+        mongo_collection: pymongo.collection.Collection
+        name (str): nom de l'école à mettre à jour
+        topics (list): liste de topics à assigner
+
+    Returns:
+        dict: résultat de l'opération update_one
     """
-    mongo_collection.uptdate_many(
-        {"name": name},
-        {"$set": {"topics": topics}}
+    return mongo_collection.update_one(
+        {"name": name},          # filtre le document à mettre à jour
+        {"$set": {"topics": topics}}  # remplace ou crée le champ 'topics'
     )
