dd2fde6e032befe9832b238132deba8fd465982e
ST-GuY <estebanlvy23@gmail.com>
2025-12-12 16:37:12 +0100

    getBudget

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear(); //récupère une fois l’année actuelle
+
+  const budget = {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+
+  return budget;
+}
