a0e460ab2bc531c6f94d77f63f45de2024bc7c0c
ST-GuY <estebanlvy23@gmail.com>
2025-12-13 18:30:00 +0100

    export default

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -13,6 +13,7 @@ export class HolbertonClass {
   }
 }
 
+
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -33,7 +34,6 @@ export class StudentHolberton {
   }
 }
 
-// Instances des classes
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -43,5 +43,7 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export du tableau
-export const listOfStudents = [student1, student2, student3, student4, student5];
+
+
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
