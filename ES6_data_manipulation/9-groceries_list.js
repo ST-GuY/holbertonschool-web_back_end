dd7702bd526830000fe699e01ab8f8578fbbae66
ST-GuY <estebanlvy23@gmail.com>
2025-12-15 16:16:11 +0100

    apples rice

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
 	const map = new Map();
-	map.set('Appel', 10);
+	map.set('Apples', 10);
 	map.set('Tomatoes', 10);
 	map.set('Pasta', 1);
-	map.set('Rize', 1);
+	map.set('Rice', 1);
 	map.set('Banana', 5);
 	return map;
 }
