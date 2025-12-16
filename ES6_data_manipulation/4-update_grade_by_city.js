aeafa37ab4e734786f33ae624e747b2ecac1cb5c
ST-GuY <estebanlvy23@gmail.com>
2025-12-16 15:48:07 +0100

    update grade by city

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,14 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-	return students
-		.filter(students => students.location === city)
-		.map(students => )
+    return students
+        .filter(student => student.location === city)
+        .map(student => {
+            const gradeObj = newGrades.find(g => g.studentId === student.id);
+            const finalGrade = gradeObj ? gradeObj.grade : 'N/A';
+            return {
+                id: student.id,
+                firstName: student.firstName,
+                location: student.location,
+                grade: finalGrade
+            };
+        });
 }
