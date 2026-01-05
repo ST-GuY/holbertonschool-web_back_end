9f3eac18d250c9572ead3b3c8821a87acb0bc809
ST-GuY <estebanlvy23@gmail.com>
2026-01-05 10:28:46 +0100

    process exit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,5 +7,7 @@ process.stdin.on('data', (data) => {
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n', () => {
+    process.exit(0);
+  });
 });
