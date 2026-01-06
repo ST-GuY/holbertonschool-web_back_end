46531e7bd98fbc3fcaf7ad1dbe79dfb15dd8b073
ST-GuY <estebanlvy23@gmail.com>
2026-01-06 17:25:29 +0100

    rename

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,41 +1,36 @@
 const fs = require('fs');
 
 const countStudents = (database) => {
-    let csv;
-    try {
-        csv = fs.readFileSync(database, { encoding: 'utf-8' });
-    } catch (err) {
-        throw new Error('Cannot load the database');
+  let csv;
+  try {
+    csv = fs.readFileSync(database, { encoding: 'utf-8' });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const lines = csv.split('\n');
+  const linesWithoutHeader = lines.slice(1);
+  const validLines = linesWithoutHeader.filter((line) => line.trim() !== '');
+  console.log(`Number of students: ${validLines.length}`);
+
+  const fields = {};
+  validLines.forEach((line) => {
+    const columns = line.split(',');
+    const firstName = columns[0];
+    const field = columns[3];
+
+    if (!fields[field]) {
+      fields[field] = [];
     }
+    fields[field].push(firstName);
+  });
 
-    // Découper en lignes et supprimer l'en-tête
-    const lignes = csv.split('\n');
-    const lignes_sans_entete = lignes.slice(1);
-
-    // Filtrer les lignes vides
-    const lignes_valides = lignes_sans_entete.filter((ligne) => ligne.trim() !== '');
-
-    // Afficher le nombre total d'étudiants
-    console.log(`Number of students: ${lignes_valides.length}`);
-
-    // Regrouper les étudiants par filière
-    const filieres = {};
-    lignes_valides.forEach((ligne) => {
-        const colonnes = ligne.split(',');
-        const first_name = colonnes[0];
-        const filiere = colonnes[3];
-
-        if (!filieres[filiere]) {
-            filieres[filiere] = [];
-        }
-        filieres[filiere].push(first_name);
-    });
-
-    // Afficher le nombre d'étudiants et la liste pour chaque filière
-    for (const filiere in filieres) {
-        const listePrenoms = filieres[filiere].join(', ');
-        console.log(`Number of students in ${filiere}: ${filieres[filiere].length}. List: ${listePrenoms}`);
+  for (const field in fields) {
+    if (fields.hasOwnProperty(field)) {
+      const listNames = fields[field].join(', ');
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${listNames}`);
     }
+  }
 };
 
 module.exports = countStudents;
