989dee5fe0251e59e46aecb92753a57cda447b93
ST-GuY <estebanlvy23@gmail.com>
2026-01-07 11:23:55 +0100

    try

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,3 +1,15 @@
-function countStudents(path) {
-	
-}
+const fs = require('fs');
+
+const countStudents = (database) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(database, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+module.exports = countStudents;
